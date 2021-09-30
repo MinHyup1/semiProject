0eaef95e9aa78c4aad5d31763b54619091aff55a
kijung <dlrlwjd1313@naver.com>
2021-09-30 10:16:31 +0900

    [dev->main 이기정]카카오 로그인 멤버수정 비 입력시 권한검사 후 이동 commit

--- src/com/kh/semi/common/filter/AuthorizationFilter.java
+++ src/com/kh/semi/common/filter/AuthorizationFilter.java
@@ -17,29 +17,30 @@ import com.kh.semi.member.model.dto.Member;
 
 public class AuthorizationFilter implements Filter {
 
-    public AuthorizationFilter() {
-        // TODO Auto-generated constructor stub
-    }
+	public AuthorizationFilter() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		String[] uriArr = httpRequest.getRequestURI().split("/");
-		
-		if(uriArr.length != 0) {
+
+		if (uriArr.length != 0) {
 			switch (uriArr[1]) {
-			/*
-			 * case "member": memberAuthorize(httpRequest, httpResponse, uriArr); break;
-			 */
+			
+			case "member": memberAuthorize(httpRequest, httpResponse, uriArr); break;
+			case "index": index(httpRequest, httpResponse, uriArr); break; 
 			case "schedule":
 				scheduleAuthorize(httpRequest, httpResponse, uriArr);
 				break;
 			case "board":
-				//boardAuthorize(httpRequest, httpResponse, uriArr);
+				// boardAuthorize(httpRequest, httpResponse, uriArr);
 				break;
 			default:
 				break;
@@ -48,75 +49,63 @@ public class AuthorizationFilter implements Filter {
 		chain.doFilter(request, response);
 	}
 
-	
-	/*
-	 * private void boardAuthorize(HttpServletRequest httpRequest,
-	 * HttpServletResponse httpResponse, String[] uriArr) {
-	 * 
-	 * HttpSession session = httpRequest.getSession();
-	 * Member member = (Member) session.getAttribute("authentication");
-	 * 
-	 * switch (uriArr[2]) {
-	 * case "board-form":
-	 * if (member == null) {
-	 * throw new HandlableException(ErrorCode.UNAUTHORIZED_PAGE_ERROR);
-	 * }
-	 * break;
-	 * case "upload":
-	 * if (member == null) {
-	 * throw new HandlableException(ErrorCode.UNAUTHORIZED_PAGE_ERROR);
-	 * }
-	 * break;
-	 * default:
-	 * break;
-	 * }
-	 * }
-	 */
-	 
 
-	private void scheduleAuthorize(HttpServletRequest httpRequest, HttpServletResponse httpResponse, String[] uriArr) {
-		
+	private void index(HttpServletRequest httpRequest, HttpServletResponse httpResponse, String[] uriArr) throws ServletException, IOException {
 		HttpSession session = httpRequest.getSession();
 		Member member = (Member) session.getAttribute("authentication");
 		
-		if(member == null) {
-			throw new HandlableException(ErrorCode.NOT_MEMBER_ERROR);
+		if(member==null) {
+			httpRequest.getRequestDispatcher("/index").forward(httpRequest, httpResponse);
+		}else if(member.getKakaoNum()==1 &&member.getPhone()==null&&member.getName()==null) {
+			 throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/kakaoMemberForm"));
 		}
+
 		
+			 
 	}
 
-	
-	/*
-	 * private void memberAuthorize(HttpServletRequest httpRequest,
-	 * HttpServletResponse httpResponse, String[] uriArr) {
-	 * 
-	 * HttpSession session = httpRequest.getSession();
-	 * 
-	 * switch (uriArr[2]) { case "changeForm":
-	 * if(session.getAttribute("authentication") == null) { throw new
-	 * HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage")); } case
-	 * "delete": if(session.getAttribute("authentication") == null) { throw new
-	 * HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage")); } case
-	 * "change": if(session.getAttribute("authentication") == null) { throw new
-	 * HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage")); } case
-	 * "id-check": if(session.getAttribute("authentication") == null) { throw new
-	 * HandlableException(ErrorCode.REDIRECT.setURL("/member/login-form")); } break;
-	 * case "nickName-check": if(session.getAttribute("authentication") == null) {
-	 * throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
-	 * } case "phone-check": if(session.getAttribute("authentication") == null) {
-	 * throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
-	 * } case "email-check": if(session.getAttribute("authentication") == null) {
-	 * throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
-	 * } case "kakaoChange": if(session.getAttribute("authentication") == null) {
-	 * throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
-	 * } case "kakaoMemberForm": if(session.getAttribute("authentication") == null)
-	 * { throw new
-	 * HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage")); } break;
-	 * default: break; }
-	 * 
-	 * }
-	 */
-	 
+	private void scheduleAuthorize(HttpServletRequest httpRequest, HttpServletResponse httpResponse, String[] uriArr) {
+
+		HttpSession session = httpRequest.getSession();
+		Member member = (Member) session.getAttribute("authentication");
+
+		if (member == null) {
+			throw new HandlableException(ErrorCode.NOT_MEMBER_ERROR);
+		}
+
+	}
+
+	private void memberAuthorize(HttpServletRequest httpRequest, HttpServletResponse httpResponse, String[] uriArr) {
+
+		HttpSession session = httpRequest.getSession();
+
+		switch (uriArr[2]) {
+		case "changeForm":
+			if (session.getAttribute("authentication") == null) {
+				throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
+			}
+		case "delete":
+			if (session.getAttribute("authentication") == null) {
+				throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
+			}
+		case "change":
+			if (session.getAttribute("authentication") == null) {
+				throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
+			}
+		case "kakaoChange":
+			if (session.getAttribute("authentication") == null) {
+				throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
+			}
+		case "kakaoMemberForm":
+			if (session.getAttribute("authentication") == null) {
+				throw new HandlableException(ErrorCode.REDIRECT.setURL("/member/loginPage"));
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
